66bc091426ee2150ca4b9cf0d7bbbc146aacbb75
guoxk <gxk1744413631@163.com>
2026-01-25 22:32:01 +0800

    feat: add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: process.env.NODE_ENV === 'production',
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(port);
 }
-bootstrap();
+bootstrap()
+  .then(() => {
+    console.log('Application is running on port', process.env.PORT || 3000);
+  })
+  .catch((err) => {
+    console.error('Error during application bootstrap', err);
+  });
